4201838f704469b6cf62f779438fcb2ed12b44ea
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-06-22 18:03:49 +0000

    added devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Grams::Application.configure do
+
+  config.action_mailer.default_url_options = { host: 'grams-charlie-ohara.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
