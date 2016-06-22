d5498e982c7d174b4b74a76f04be384e6f94e9c1
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-06-22 13:45:53 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grams::Application.config.secret_key_base = '72f91468907d9628fb41e4bb062f39616efcb3f7b57e796cfa7146ac83b0ccc34da9acb8f4a69fed13538b1036ac52bed5dce969e4fd9b09ca516e66b270ce2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grams::Application.config.session_store :cookie_store, key: '_grams_session'
